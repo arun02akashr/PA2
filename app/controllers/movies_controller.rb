3bd296518777bbcaa4a5e503f40411751cc274e5
arun02akashr <arun_akashr@tamu.edu>
2025-02-12 23:31:23 -0600

    Ensure MoviesController responds to HTML

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 class MoviesController < ApplicationController
     def index
       @movies = Movie.all
+      respond_to do |format|
+      format.html  # Ensures the server responds to HTML requests
+      format.json { render json: @movies }
     end
 end
   
