bfb010c56f5b08e076c264b8fae3f4c58c30ac75
arun02akashr <arun_akashr@tamu.edu>
2025-02-12 23:39:09 -0600

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,22 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  { title: "The Godfather", rating: "R", description: "Mafia family drama", release_date: "1972-03-24" },
+  
+  { title: "Inception", rating: "PG-13", description: "Dream within a dream", release_date: "2010-07-16" },
+  
+  { title: "Titanic", rating: "PG-13", description: "Love story on the Titanic", release_date: "1997-12-19" },
+  
+  { title: "The Matrix", rating: "R", description: "Virtual reality revolution", release_date: "1999-03-31" },
+  
+  { title: "Interstellar", rating: "PG-13", description: "Space travel through wormholes", release_date: "2014-11-07" }
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+    Movie.find_or_create_by(title: movie[:title]) do |m|
+      m.rating = movie[:rating]
+      m.description = movie[:description]
+      m.release_date = movie[:release_date]
+    end
 end
